ec3fdb87a64373bf967aac5bdf3c05e48afc816c
DESKTOP-T8B5N82\sanShao <tang5011235@163.com>
2017-11-23 10:33:44 +0800

    添加依赖

--- app/src/main/java/test/myapplication/MainActivity.java
+++ app/src/main/java/test/myapplication/MainActivity.java
@@ -1,7 +1,14 @@
 package test.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.List;
+
+import okhttp3.Cookie;
+import okhttp3.CookieJar;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CookieJar cookieJar = new CookieJar() {
+            @Override
+            public void saveFromResponse(HttpUrl url, List<Cookie> cookies) {
+
+            }
+
+            @Override
+            public List<Cookie> loadForRequest(HttpUrl url) {
+                return null;
+            }
+        }
+        OkHttpClient.Builder builder = new OkHttpClient.Builder()
+                .cookieJar();
     }
 }
